3376280d90327b14bc308db3b75fb1ccd91183d2
Seth Haigh <webdev>
2022-03-24 17:06:04 -0700

    add partials

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", function(req, res) {
+  res.render("home",{startingContent: homeStartingContent});
+});
 
 
 app.listen(3000, function() {
