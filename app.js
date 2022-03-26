5674befaa30dff3f6b956bd6d0ff35ef69f63c4f
Seth Haigh <webdev>
2022-03-25 17:36:27 -0700

    add compose page functionality

--- app.js
+++ app.js
@@ -12,19 +12,41 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  res.render("home",{startingContent: homeStartingContent});
+  res.render("home", {
+    startingContent: homeStartingContent
+  });
 });
 
 app.get("/about", function(req, res) {
-  res.render("about",{aboutContent: aboutContent});
+  res.render("about", {
+    aboutContent: aboutContent
+  });
 });
 
 app.get("/contact", function(req, res) {
-  res.render("contact",{contactContent: contactContent});
+  res.render("contact", {
+    contactContent: contactContent
+  });
+});
+
+app.get("/compose", function(req, res) {
+  res.render("compose", {});
+});
+
+app.post("/compose", function(req, res) {
+  const blogPosts = {
+    title: req.body.postTitle,
+    body: req.body.postBody
+  };
+
+  console.log(blogPosts);
+  res.redirect("/compose");
 });
 
 app.listen(3000, function() {
